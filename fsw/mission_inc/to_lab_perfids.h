33c0f7d6012681e76b07477f7ad6d4998582056d
avan989 <avan989@yahoo.com>
2019-08-23 16:42:25 -0400

    Remove MKS flags

--- fsw/mission_inc/to_lab_perfids.h
+++ fsw/mission_inc/to_lab_perfids.h
@@ -18,22 +18,12 @@
 **      See the License for the specific language governing permissions and
 **      limitations under the License.
 **
-** File:
+** File: to_lab_perfids.h
 **
 ** Purpose: 
 **  Define TO Lab Performance IDs
 **
 ** Notes:
-**
-** $Log: to_lab_perfids.h  $
-** Revision 1.3 2010/09/20 12:28:05GMT-05:00 wmoleski 
-** Modified the CI_LAB, SCH_LAB and TO_LAB applications to use unique message IDs and Pipe Names. The "_LAB"
-** was added to all definitions so that a mission can use these "Lab" apps as well as their own mission apps together.
-** Revision 1.2 2008/04/30 14:43:01EDT rjmcgraw 
-** Member moved from to_lab_perfids.h in project c:/MKSDATA/MKS-REPOSITORY/CFS-REPOSITORY/to_lab/fsw/platform_inc/project.pj to to_lab_perfids.h in project c:/MKSDATA/MKS-REPOSITORY/CFS-REPOSITORY/to_lab/fsw/mission_inc/project.pj.
-** Revision 1.1 2008/04/30 13:43:01ACT rjmcgraw 
-** Initial revision
-** Member added to project c:/MKSDATA/MKS-REPOSITORY/CFS-REPOSITORY/to_lab/fsw/platform_inc/project.pj
 **
 *************************************************************************/
 #ifndef _to_lab_perfids_h_
